cfa3b35447d8afa68f92d993ebfa12910435d4d8
MemeBoiSwoo <46385426+MemeBoiSwoo@users.noreply.github.com>
2022-05-18 22:16:24 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+// function myFunction(x) {
+//     if (x.matches) { // If media query matches
+//       document.getElementById("nav-img").innerHTML = "Work";
+//     }
+//   }
+
+//   var x = window.matchMedia("(max-width: 600px)")
+//   myFunction(x) // Call listener function at run time
+//   x.addEventListener('resize',myFunction) // Attach listener function on state changes 
+// const video = document.getElementById("video-background");
+// window.onload = setInterval(pauseVideo(), 30000);
+
+// myStopFunction()
+
+// function pauseVideo() {
+//     video.setAttribute("src", "https://www.youtube.com/embed/amJ8anzZqf4?playlist=amJ8anzZqf4&loop=0&autoplay=1&mute=1");
+// }
+
+// function myStopFunction() {
+//     clearTimeout(myInterval);
+// }
